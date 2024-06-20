bbc094bc39dc666c7dc76621e0048986304caaaf
Stas Ukolov <ukoloff@gmail.com>
2024-06-20 11:53:19 +0500

    Fetch list of X509

--- src/fetch.py
+++ src/fetch.py
@@ -1,5 +1,17 @@
+#
+# Fetch X509 certificates from NetAngels.ru
+#
 from os import path
 import requests
 
-token = open(path.join(path.dirname(__file__), '.token')).readline().strip()
-print(token)
+API = 'https://api-ms.netangels.ru/api/v1/'
+
+api_key = open(path.join(path.dirname(__file__), '.token')).readline().strip()
+
+s = requests.Session()
+
+token = s.post('https://panel.netangels.ru/api/gateway/token/', data={'api_key': api_key}).json()['token']
+s.headers['authorization'] = 'Bearer ' + token
+
+x509s = s.get(API + 'certificates/').json()
+print(x509s)
