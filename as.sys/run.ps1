ce846a4c5526fea330d58067250c87e2eed6c45b
Stas Ukolov <ukoloff@gmail.com>
2024-06-25 11:36:48 +0500

    Add simple Task

--- as.sys/run.ps1
+++ as.sys/run.ps1
@@ -1,1 +1,6 @@
-powershell -c "Get-Date | Set-Content 'C:/Temp/out.txt'"
+# powershell -c "Get-Date | Set-Content 'C:/Temp/out.txt'"
+
+$Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c `"Get-Date | Set-Content 'C:/Temp/out.txt'`""
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger
+Register-ScheduledTask -TaskName "TestX" -InputObject $Task -Force
