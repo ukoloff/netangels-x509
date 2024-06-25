f819990f203760417873194244f158f0dcc597df
Stas Ukolov <ukoloff@gmail.com>
2024-06-25 14:02:09 +0500

    Auto-remove Task

--- as.sys/run.ps1
+++ as.sys/run.ps1
@@ -2,7 +2,7 @@
 
 $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c `" whoami | Set-Content 'C:/Temp/out.txt'`""
 $Trigger = Get-CimClass -ClassName 'MSFT_TaskRegistrationTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler'
-New-ScheduledTaskTrigger -Once -At (Get-Date)
-$Task = New-ScheduledTask -Action $Action -Trigger $Trigger
-$Options = New-ScheduledTaskSettingsSet -Priority 5 #-StartWhenAvailable
+$Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:00:01
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+$Task.Triggers[0].EndBoundary = (Get-Date).AddSeconds(5).ToString('s')
 Register-ScheduledTask -TaskName "Now!" -TaskPath uxm -InputObject $Task -User "System" -Force
