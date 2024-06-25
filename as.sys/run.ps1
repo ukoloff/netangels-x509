9783e7f213b9af44a0fbdb2ec675ef447e49cb1f
Stas Ukolov <ukoloff@gmail.com>
2024-06-25 11:55:08 +0500

    Run Task immediately

--- as.sys/run.ps1
+++ as.sys/run.ps1
@@ -1,6 +1,8 @@
 # powershell -c "Get-Date | Set-Content 'C:/Temp/out.txt'"
 
 $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c `"Get-Date | Set-Content 'C:/Temp/out.txt'`""
-$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+$Trigger = Get-CimClass -ClassName 'MSFT_TaskRegistrationTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler'
+New-ScheduledTaskTrigger -Once -At (Get-Date)
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger
-Register-ScheduledTask -TaskName "TestX" -TaskPath uxm -InputObject $Task -Force
+$Options = New-ScheduledTaskSettingsSet -Priority 5 #-StartWhenAvailable
+Register-ScheduledTask -TaskName "Now!" -TaskPath uxm -InputObject $Task -Force
